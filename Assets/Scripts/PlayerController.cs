833f9f28896dd295d2007f7576fa75223c1b94c6
Realitaetsverlust <realitaetsverlust@realitaetsverlust.rocks>
2019-09-21 14:34:42 +0200

    The elder scrolls foretold that the dragonborn will start the impplementation of range finding

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.TerrainAPI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private GameObject _position;
+    private int _walkRange;
+
+    public void displayWalkRange() {
+        
+    }
+}
