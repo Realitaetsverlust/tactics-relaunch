5af25f39df450a17d29cef28c47a6cdc85adf750
Realitätsverlust <realitaetsverlust@realitaetsverlust.rocks>
2019-12-29 19:56:04 +0100

    The elder scrolls foretold that the dragonborn will add new damage types to basemove

--- Assets/Scripts/Moves/BaseMove.cs
+++ Assets/Scripts/Moves/BaseMove.cs
@@ -1,11 +1,22 @@
+using System.Runtime.CompilerServices;
+
 namespace Moves {
-	public class BaseMove {
+	public abstract class BaseMove {
 		protected int baseDamage;
-		protected int damageType; //1 = phys; 2 = mag;
+		protected int damageType; //1 = phys; 2 = mag; 3 = heal; 4 = statusheal; 5 = terraineffect
 		protected int range;
 		protected int mpCost;
 		protected string name;
 		protected string description;
-		
+
+		public BaseMove() {
+			this.setName();
+		}
+
+		protected void setName() {
+			foreach(char character in this.GetType().Name.ToCharArray()) {
+				
+			} 
+		}
 	}
 }
