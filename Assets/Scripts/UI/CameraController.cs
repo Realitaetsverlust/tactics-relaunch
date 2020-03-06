fc89d5afc5931296cc745428b338df68febc270e
Kevin Soldner <kevin.soldner@dotfly.de>
2020-03-06 13:36:19 +0100

    The elder scrolls foretold that the dragonborn will add camera rotation on button press (small bug, it jumps a bit too far)

--- Assets/Scripts/UI/CameraController.cs
+++ Assets/Scripts/UI/CameraController.cs
@@ -1,47 +1,68 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using Utils;
 
 namespace UI {
     public class CameraController : MonoBehaviour {
+        public float rotationSpeed = 200;
+        public float minRotation = -90;
+        public float maxRotation = 90;
+        
         private Camera _main;
         private GameObject _target;
         private Vector3 _mainCameraPosition;
         private float _mainCameraRotation;
         private bool _cameraMoveInProgress;
-        private short _rotationDirection = 0; // 0 = no rotation, -1 = left, 1 = right
+        private sbyte _rotationDirection = 0;
+        private float _elapsedRotation;
         
         private void Start() {
             this._main = Camera.main;
         }
 
         public void Update() {
-            if(this._cameraMoveInProgress == false) {
+            if(!this._cameraMoveInProgress) {
                 if(Input.GetKeyDown(KeyCode.Q)) {
                     this._cameraMoveInProgress = true;
-                    
+                    this._rotationDirection = 1;
                 }
 
                 if(Input.GetKeyDown(KeyCode.E)) {
                     this._cameraMoveInProgress = true;
-                    this.rotateRight(TurnOrder.getActiveCharacter());
+                    this._rotationDirection = -1;
                 }
+            }
+
+            if(this._rotationDirection == 1) {
+                this.rotateLeft(TurnOrder.getActiveCharacter());
+            }
+
+            if(this._rotationDirection == -1) {
+                this.rotateRight(TurnOrder.getActiveCharacter());
             }
         }
 
         public void setCameraToCharacter(GameObject character) {
             Vector3 characterPosition = character.transform.position;
             
-            this._main.transform.position = new Vector3(characterPosition.x - 5f, 
-                                                       characterPosition.y + 10f, 
+            this._main.transform.position = new Vector3(characterPosition.x - 5f,
+                                                       characterPosition.y + 10f,
                                                        characterPosition.z + 5f);
         }
 
-        public void rotateLeft(GameObject character) { 
-            //this.transform.RotateAround (character.transform.position, Vector3.up, 90);
+        public void rotateLeft(GameObject character) {
+            float rotation = Time.deltaTime * 200f;
 
-            this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, Quaternion.Euler(0, 90, 0), 10f * Time.deltaTime);
+            if(this._elapsedRotation >= 90f) {
+                rotation = 90f - this._elapsedRotation;
+                this._cameraMoveInProgress = false;
+                this._rotationDirection = 0;
+                this._elapsedRotation = 0;
+            }
+            
+            Debug.Log(rotation);
 
+            this.transform.RotateAround (character.transform.position, Vector3.up, rotation);
+            this._elapsedRotation += rotation;
         }
 
         public void rotateRight(GameObject character) {
