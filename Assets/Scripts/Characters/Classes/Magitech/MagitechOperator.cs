cf9341d37c637df1fc678423e841f823f2a25975
Realitätsverlust <realitaetsverlust@realitaetsverlust.rocks>
2020-05-30 13:00:33 +0200

    The elder scrolls foretold that the dragonborn will add a few new classes

---
+++ Assets/Scripts/Characters/Classes/Magitech/MagitechOperator.cs
@@ -0,0 +1,8 @@
+namespace Characters.Classes.Magitech {
+	public class MagitechOperator : ClassBase {
+		public MagitechOperator() : base() {
+			this.name = "Magitech Operator";
+			this.description = "Capable of controlling ancient Mechs, Magitech Operators are extremely powerful and durable.";
+		}
+	}
+}
