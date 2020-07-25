f3d1bfbc8e06f667d0a0bd82af0eac7d955aa581
Realitaetsverlust <realitaetsverlust@realitaetsverlust.rocks>
2020-07-25 20:55:02 +0200

    I'll stop with the elder scrolls stuff now. It's getting annoying. Also, added the metafiles again because unity apparently needs those initially. Good thing I have no fucking idea how unity works. :^   )

---
+++ Assets/Scripts/Global/Team.cs
@@ -0,0 +1,17 @@
+namespace Global {
+    public class Team {
+        private readonly string teamFileLocation = "saves/team.json";
+
+        public void load() {
+            
+        }
+
+        public void test() {
+            
+        }
+        
+        public void save() {
+                        
+        }
+    }
+}
